9ba9972fa3c5d38d62dfeb1b87e7d2e6d5612218
ChristianChl <christianroberth.ch@gmail.com>
2021-08-16 11:28:38 -0500

    commit steps finish modulos

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -4,12 +4,7 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize('bdtelenor' , 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
-<<<<<<< HEAD
-    // port: 3307
-=======
-    port: 3307
-    
->>>>>>> 14dddfaf1f9c30e874e980489cddbbddca610188
+    // port: 3307,
     //logggin: false,
 });
 
