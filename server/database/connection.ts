76193e5d4468678af4c0063293fee05ee820e9ab
ChristianChl <christianroberth.ch@gmail.com>
2021-08-30 20:09:54 -0500

    commit conection update

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,19 +1,21 @@
 import { Sequelize } from "sequelize";
-// const db = new Sequelize('bdtelenor' , 'root', '', {
-//     host: 'localhost',
-//     dialect: 'mysql',
-    // port: 3307,
-    //logggin: false,
-// });
 
-// const db = new Sequelize('nrsi45pnjgylc4wv' , 'u03804lj5kn69i3t', 'skx7rs5pgfpq5tr5', {
-//     host: 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+const db = new Sequelize('bdtelenor' , 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    // port: 3307,
+    // logggin: false,
+});
+
+//PRO
+
+// const db = new Sequelize(process.env.DB_DATABASE||'nrsi45pnjgylc4wv' , process.env.DB_USER||'u03804lj5kn69i3t', process.env.DB_PASSORD||'skx7rs5pgfpq5tr5', {
+//     host: process.env.DB_HOST||'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
 //     dialect: 'mysql',
 //     logging: true,
 //     // port: 3307,
 // });
 
-<<<<<<< HEAD
 //DEV ÑAÑO
 
 // const db = new Sequelize(process.env.DB_DATABASE || 'bd_new_segunda' ,process.env.DB_USER || 'root', '', {
@@ -21,15 +23,5 @@ import { Sequelize } from "sequelize";
 //     dialect:'mysql',
 //     port: 3307,
 // });
-=======
-
-
-const db = new Sequelize('bd_new_segunda' , 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3307,
-    // logggin: false,
-});
->>>>>>> 3bd09da7e5b74d9244a8d60a84662c9234b64799
 
 export default db;
