b829abccd4649539718dcfd6b32cec363cc114ea
ChristianChl <christianroberth.ch@gmail.com>
2021-08-30 20:22:13 -0500

    commit update connection

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -16,7 +16,6 @@ const db = new Sequelize('bdtelenor' , 'root', '', {
 //     // port: 3307,
 // });
 
-<<<<<<< HEAD
 //DEV ÑAÑO
 
 // const db = new Sequelize(process.env.DB_DATABASE || 'bd_new_segunda' ,process.env.DB_USER || 'root', '', {
@@ -24,13 +23,5 @@ const db = new Sequelize('bdtelenor' , 'root', '', {
 //     dialect:'mysql',
 //     port: 3307,
 // });
-=======
-const db = new Sequelize(process.env.DB_DATABASE || 'bd_new_segunda' ,'root', '', {
- host: process.env.DB_HOST ||'localhost',
-dialect:'mysql',
-port: 3307,
-});
-
->>>>>>> 680b566ca8f7333d022e5790a85f8dd9b8466c38
 
 export default db;
