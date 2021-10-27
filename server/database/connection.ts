4451d2788a1654da9c1e3215e239f0e67ed26fa1
ChristianChl <christianroberth.ch@gmail.com>
2021-10-27 17:24:49 -0500

    commit version ultiomate beta web telenor

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,20 +1,20 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('bdtelenor' , 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    // port: 3307,
-    // logggin: false,
-});
+// const db = new Sequelize('bdtelenor' , 'root', '', {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3307,
+//     logggin: false,
+// });
 
 //PRO
 
-// const db = new Sequelize(process.env.DB_DATABASE||'nrsi45pnjgylc4wv' , process.env.DB_USER||'u03804lj5kn69i3t', process.env.DB_PASSORD||'skx7rs5pgfpq5tr5', {
-//     host: process.env.DB_HOST||'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     dialect: 'mysql',
-//     logging: true,
-//     // port: 3307,
-// });
+const db = new Sequelize(process.env.DB_DATABASE||'nrsi45pnjgylc4wv' , process.env.DB_USER||'u03804lj5kn69i3t', process.env.DB_PASSORD||'skx7rs5pgfpq5tr5', {
+    host: process.env.DB_HOST||'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    logging: true,
+    // port: 3307,
+});
 
 //DEV ÑAÑO
 
